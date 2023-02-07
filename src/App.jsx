3efc67759a7ffd2ef3119d011561d9fbf8495031
jimoh yusuf <expensive7832@gmail.com>
2023-02-07 18:24:01 +0100

    page added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Spinner } from "reactstrap"
+
+const Home = React.lazy(() => import("./Pages/Home"))
+const Signup = React.lazy(() => import("./Pages/SIgnup"))
+const Login = React.lazy(() => import("./Pages/Login"))
+
+
+
+function App(){
+    return(
+        <React.Suspense fallback={
+            <div className="position-absolute top-50 start-50 translate-middle text-danger">
+                <Spinner type="grow"/>
+            </div>
+        }>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Home/> }/>
+                    <Route path="/signup" element={ <Signup/> }/>
+                    <Route path="/login" element={ <Login/> }/>
+                </Routes>
+            </BrowserRouter>
+        </React.Suspense>
+    )
+}
+
+
+export default App
